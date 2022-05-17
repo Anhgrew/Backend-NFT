01113cdd51a1e5ef3cfdf4a76cdf70e471213b3e
Nam Dang <nam.dang@saleshood.com>
2022-05-17 23:47:42 +0700

    Add env file

--- app/routers/item.py
+++ app/routers/item.py
@@ -5,7 +5,9 @@ from math import dist
 from fastapi import File, UploadFile, APIRouter, HTTPException
 from pathlib import Path
 from service import read_csv_file, read_pkl_file, handle_uploaded_image, get_model_predicted_results, FeatureExtractor
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Create Fast API route instance
 router = APIRouter()
@@ -17,9 +19,7 @@ NFT_API_URL = "https://api.rarible.org/v0.1/items"
 # Declare loaded images array
 feature_pkl_path = 'vector_pca.pkl'
 features = []
-# Declare loaded images path array
-path_pkl_path = 'paths_pca.pkl'
-img_paths = []
+
 # Declare loaded image token array
 token_pkl_path = 'token_pca.pkl'
 nft_tokens = []
@@ -30,7 +30,6 @@ feature_extractor = FeatureExtractor()
 # Read images from extracted folder and load to arrays
 ## Note: each token, filename, features vector is identified by the order in the array
 features = read_pkl_file(feature_pkl_path)
-img_paths = read_pkl_file(path_pkl_path)
 nft_tokens = read_pkl_file(token_pkl_path)
 
 # POST image api
@@ -62,7 +61,7 @@ async def post_image(file: UploadFile = File(...)):
         # Get top similar item from model and return result from NFT market api
 
         responses = get_model_predicted_results(ids, nft_tokens, NFT_API_URL)
-        
+
     except Exception as exception:
         # Catch error
         raise HTTPException(status_code=404, detail=exception)
