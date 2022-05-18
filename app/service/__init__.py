524edf427648cb7ecb9ff11600cb7b5699a8b12d
Nam Dang <nam.dang@saleshood.com>
2022-05-18 10:21:29 +0700

    Update code Backend run with model

--- app/service/__init__.py
+++ app/service/__init__.py
@@ -2,5 +2,5 @@ from .api_handle import get_nft_by_id as get_nft_by_id
 from .feature_extractor import FeatureExtractor as FeatureExtractor
 from .file_handle import read_csv_file
 from .file_handle import read_pkl_file
-from .image_upload_handle import handle_uploaded_image as handle_uploaded_image
-from .model_results_processing import get_model_predicted_results as get_model_predicted_results
+from .image_upload_handle import handle_uploaded_image
+from .model_results_processing import get_model_predicted_results
