3a1ad3a8d5e892f8cde0e939af36f1e76d3e27a0
Binh <54803422+KR913@users.noreply.github.com>
2022-05-21 14:48:17 +0700

    process error

--- app/service/model_results_processing.py
+++ app/service/model_results_processing.py
@@ -10,12 +10,32 @@ def get_model_predicted_results(nft_tokens, market_url):
         nft_req = get_nft_by_id(market_url, nft_token)
         response = nft_req.json()
         item = Item()
-        item.id = response["id"]
-        item.creators = response["creators"]
-        item.owners = response["owners"]
-        item.meta_name = response["meta"]["name"]
-        item.meta_description = response["meta"]["description"]
-        item.meta_content_url = response["meta"]["content"][0]["url"]
+        if "id" in response:
+            item.id = response["id"]
+        else:
+            item.id = "?"
+        if "creators" in response:
+            item.creators = response["creators"]
+        else:
+            item.creators = "?"
+        if "owners" in response:
+            item.owners = response["owners"]
+        else:
+            item.owners = "?"
+        if "meta" in response:
+            sub_response = response["meta"]
+            if "name" in sub_response:
+                item.meta_name = response["meta"]["name"]
+            else:
+                item.meta_name = "?"
+            if "description" in sub_response:
+                item.meta_description = response["meta"]["description"]
+            else:
+                item.meta_description = "?"
+            if "content" in sub_response:
+                item.meta_content_url = response["meta"]["content"][0]["url"]
+            else:
+                item.meta_content_url = "?"
         if response.get("lastSale") is not None:
             item.lastsale_price = response["lastSale"]["price"]
         else:
