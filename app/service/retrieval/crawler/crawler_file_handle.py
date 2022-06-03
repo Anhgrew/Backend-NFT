e0ac832f7b05025814d4d7606dc4d7f96680d390
Binh <54803422+KR913@users.noreply.github.com>
2022-06-03 07:40:29 +0700

    Revert "Revert "Add info crawler""
    This reverts commit 3a1f3cceb2b58520aae8ff131e0c08ada4e4f01a.

---
+++ app/service/retrieval/crawler/crawler_file_handle.py
@@ -0,0 +1,26 @@
+import requests
+import os
+import os.path as pth
+import pandas as pd
+import csv
+
+def read_crawler_file(my_path):
+  if not pth.exists(my_path):
+    my_file = open(my_path, 'w+')
+    my_file.close()
+  my_result = {}
+  if os.stat(my_path).st_size > 0:
+    with open(my_path, mode='r') as inp:
+      reader = csv.reader(inp)
+      my_result = {rows[0]:rows[1] for rows in reader}
+  return my_result
+
+def write_crawler_file(my_path, my_data):
+  if not pth.exists(my_path):
+    my_file = open(my_path, 'w+')
+    my_file.close()
+  with open(my_path, 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in my_data.items():
+      writer.writerow([key, value])
+
