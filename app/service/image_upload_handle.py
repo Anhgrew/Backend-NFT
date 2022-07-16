11b8ba7ea0e0c181856fb7c89e7327d783f0dea3
Nguyen Tien Anh <47881661+Anhgrew@users.noreply.github.com>
2022-07-17 05:52:50 +0700

    fix: model image mode (#12)

--- app/service/image_upload_handle.py
+++ app/service/image_upload_handle.py
@@ -5,6 +5,11 @@ from datetime import datetime
 # Handle uploaded image and save to local folder
 def handle_uploaded_image(contents, file):
     image = Image.open(io.BytesIO(contents))
+    image.load()
+
+    # replace alpha channel with white color
+    image = Image.new('RGB', image.size, (255, 255, 255))
+    image.paste(image, None)
     uploaded_img_path = (
         "static/uploaded/"
         + datetime.now().isoformat().replace(":", ".")
