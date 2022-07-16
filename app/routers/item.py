cbeb20a41f980834667b2ddd9421286001c2048e
Anhgrew <socksockmoizui@gmail.com>
2022-07-17 05:52:07 +0700

    fix: model image mode

--- app/routers/item.py
+++ app/routers/item.py
@@ -100,7 +100,10 @@ async def post_image(data: bytes = Depends(parse_body)):
     try:
         # Read content of uploaded image
         img_search = Image.open(io.BytesIO(data))
-
+        # replace alpha channel with white color
+        print("CCCC", img_search)
+        img_search = Image.new('RGB', img_search.size, (255, 255, 255))
+        img_search.paste(img_search, None)
         #  Executed model start time
         start = time.time()
 
